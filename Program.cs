e687f12e7df5be35c025ce25f0941dbf0fcdda89
DaigoYoshimura <daigo1020daigo@gmail.com>
2023-04-26 15:28:00 +0900

    Second commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class Car
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+        }
+    }
+}
